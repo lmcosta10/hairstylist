a19600370f7d1a5a2efb20ba72cde0454cef0f14
lmcosta10 <letmc14@gmail.com>
2024-12-16 00:45:00 -0300

    face detector from datacamp

--- hairstylist.py
+++ hairstylist.py
@@ -1,9 +1,26 @@
 import cv2
+import matplotlib.pyplot as plt
 
-cap = cv2.VideoCapture(0)
+imagePath = 'person.jpg'
 
-while True:
-    success, img = cap.read()
+img = cv2.imread(imagePath)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(img_rgb)
+plt.axis('off')
+plt.show()
